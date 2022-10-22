0d2b6f34c43dcd23acd397b9c568e101d6ceed37
design-px <you@example.com>
2022-10-22 22:48:16 +0530

    first commit

---
+++ app/js/script.js
@@ -0,0 +1,5 @@
+//elements
+const form = document.getElementById('todoform');
+const todoInput = document.getElementById('newtodo');
+const todosListEl = document.getElementById('todos-list');
+const notification = document.querySelector('.notification');
